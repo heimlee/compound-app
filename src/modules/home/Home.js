b9d3885aa69f17c65d1d0b05880c465027b3a3d6
Andrey Leonets <awdenheimlee@gmail.com>
2021-07-27 19:31:02 +0300

    Feat (home): implement Footer component

--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -3,15 +3,19 @@ import React from 'react';
 import { HomeTitle } from './components/homeTitle/HomeTitle';
 import { HomeDescription } from './components/homeDescription/HomeDescription';
 import { TechnologyStack } from './components/technologyStack/TechnologyStack';
+import { Footer } from './components/footer/Footer';
 
 import './home.scss';
 
 export const Home = () => (
-  <main className="home-page-container">
-    <div className="home-page-wrapper">
-      <HomeTitle />
-      <HomeDescription />
-      <TechnologyStack />
-    </div>
-  </main>
+  <>
+    <main className="home-page-container">
+      <div className="home-page-wrapper">
+        <HomeTitle />
+        <HomeDescription />
+        <TechnologyStack />
+      </div>
+    </main>
+    <Footer />
+  </>
 );
--- src/modules/home/components/homeTitle/HomeTitle.js
+++ src/modules/home/components/homeTitle/HomeTitle.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import './homeTitle.scss';
+
 export const HomeTitle = () => (
   <h1 className="home-page-title">The Compound App is a space for developing, integrating and testing the React/Redux ecosystem.</h1>
 );
