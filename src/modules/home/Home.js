99f2b5ebea9833a1076016357940db32eef5a1b3
Andrey Leonets <awdenheimlee@gmail.com>
2021-07-27 15:26:52 +0300

    Refactoring: extract HomeTitle into a separate component

--- src/modules/home/Home.js
+++ src/modules/home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { HomeTitle } from './components/homeTitle/HomeTitle';
+
 import './home.scss';
 
 export const Home = () => (
   <main className="home-page-container">
     <div className="home-page-wrapper">
-      <h1 className="home-page-title">Home page</h1>
-      <p>This is the home page.</p>
+      <HomeTitle />
+      <p>The functionality of the application provides for displaying platform users, their posts and todo lists.</p>
       <div>
         <Link to="/posts" className="button">View Posts</Link>
         <Link to="/users" className="button">View Users</Link>
---
+++ src/modules/home/components/homeTitle/HomeTitle.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const HomeTitle = () => (
+  <h1 className="home-page-title">The Compound App is a space for developing, integrating and testing the React/Redux ecosystem.</h1>
+);
